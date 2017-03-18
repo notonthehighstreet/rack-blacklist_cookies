857011a20b8e5eef0b544dc08efaaa9858db40c4
Mazin Power <mazinpower@notonthehighstreet.com>
2017-03-18 17:49:37 +0000

    First green

--- lib/rack/blacklist_cookies.rb
+++ lib/rack/blacklist_cookies.rb
@@ -2,15 +2,21 @@ module Rack
   class BlacklistCookies
     def initialize(app)
       @app = app
+      @request_blacklist = BlacklistCookies.configuration.request_blacklist
       @response_blacklist = BlacklistCookies.configuration.response_blacklist
     end
 
     def call(env)
+      current_path = env["PATH_INFO"]
+
+      if @request_blacklist[current_path]
+        env["HTTP_COOKIE"] = remove_request_cookies(env["HTTP_COOKIE"], current_path)
+      end
+
       status, headers, body = @app.call(env)
 
-      if @response_blacklist[env["PATH_INFO"]]
-        @unwanted_cookie_names = @response_blacklist[env["PATH_INFO"]]
-        headers["Set-Cookie"] = remove_unwanted_cookies(headers["Set-Cookie"])
+      if @response_blacklist[current_path]
+        headers["Set-Cookie"] = remove_response_cookies(headers["Set-Cookie"], current_path)
       end
 
       [status, headers, body]
@@ -18,9 +24,17 @@ module Rack
 
     private
 
-    def remove_unwanted_cookies(set_cookie_header)
-      new_cookies_header = set_cookie_header.split("\n")
-      @unwanted_cookie_names.each do |cookie_name|
+    def remove_request_cookies(cookie_header, current_path)
+      new_cookies_header = cookie_header.split("\n")
+      @request_blacklist[current_path].each do |cookie_name|
+        new_cookies_header.reject! { |cookie| "#{cookie_name}=" == cookie[0..cookie_name.length] }
+      end
+      new_cookies_header.join("\n")
+    end
+
+    def remove_response_cookies(cookie_header, current_path)
+      new_cookies_header = cookie_header.split("\n")
+      @response_blacklist[current_path].each do |cookie_name|
         new_cookies_header.reject! { |cookie| "#{cookie_name}=" == cookie[0..cookie_name.length] }
       end
       new_cookies_header.join("\n")
--- spec/rack/blacklist_cookies_spec.rb
+++ spec/rack/blacklist_cookies_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 RSpec.describe Rack::BlacklistCookies do
   include_context "rack_setup"
-  
+
   before do
     Rack::BlacklistCookies.configure do |config|
       config.request_blacklist = {
@@ -11,6 +11,39 @@ RSpec.describe Rack::BlacklistCookies do
       config.response_blacklist = {
         "/" => ["unwanted_cookie"],
       }
+    end
+  end
+
+  context "stripping the cookies off the request" do
+    let(:subject) { described_class.new(app) }
+    let(:request_cookies) do
+      "unwanted_cookie=DELETEME; domain=.example.com; path=/\n"\
+      "preferences=true; path=/\n"\
+      "country=USA; path=/; expires=Sat, 17 Mar 2020 21:00:00 -0000\n"\
+      "session=latest; domain=.open.example.com; path=/; HttpOnly"
+    end
+    let(:cleaned_request_cookie) do
+      "preferences=true; path=/\n"\
+      "country=USA; path=/; expires=Sat, 17 Mar 2020 21:00:00 -0000\n"\
+      "session=latest; domain=.open.example.com; path=/; HttpOnly"
+    end
+
+    context "and the path has been defined" do
+      let(:request_path) { "/" }
+
+      it "removes the unwanted cookie and leaves the others" do
+        expect(subject).to receive(:remove_request_cookies).with(request_cookies, request_path)
+        subject.call(env)
+      end
+    end
+
+    context "and the path has not been defined" do
+      let(:request_path) { "/bananas" }
+
+      it "does not remove any cookies" do
+        expect(subject).to_not receive(:remove_request_cookies)
+        subject.call(env)
+      end
     end
   end
 
@@ -25,7 +58,7 @@ RSpec.describe Rack::BlacklistCookies do
       "session=latest; domain=.open.example.com; path=/; HttpOnly"
     end
 
-    let(:stripped_response_cookies) do
+    let(:cleaned_response_cookie) do
       "preferences=true; path=/\n"\
       "country=USA; path=/; expires=Sat, 17 Mar 2020 21:00:00 -0000\n"\
       "session=latest; domain=.open.example.com; path=/; HttpOnly"
@@ -35,7 +68,7 @@ RSpec.describe Rack::BlacklistCookies do
       let(:request_path) { "/" }
 
       it "removes the unwanted cookie and leaves the others" do
-        expect(headers["Set-Cookie"]).to eq(stripped_response_cookies)
+        expect(headers["Set-Cookie"]).to eq(cleaned_response_cookie)
       end
     end
 
